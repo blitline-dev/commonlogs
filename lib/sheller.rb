d43e5b6d95cdaaa8c85aa640ee7b84e13608bb4d
Jason Malcolm <jaciones@gmail.com>
2016-06-08 16:08:04 -0700

    Fix for encoding problems on sheller

--- lib/sheller.rb
+++ lib/sheller.rb
@@ -2,35 +2,41 @@ require 'open3'
 
 # Extender for running shell commands
 module Sheller
-	MAX_LIST_SIZE = 50_000
+  MAX_LIST_SIZE = 50_000
 
-	def execute_shell_command(cmd_string, with_context = false)
-		results = nil
-		start_time = Time.now.to_i
-		p "Start cmd '#{cmd_string}'"
-		Open3.popen3(cmd_string) do |_stdin, stdout, stderr, _wait_thr|
-			output = stdout.read
-			output_error = stderr.read
-			handle_output_error(output_error)
-			results = parse_results(output, with_context)
-		end
-		end_time = Time.now.to_i
-		puts "Finished after: #{end_time - start_time} seconds"
+  def execute_shell_command(cmd_string, with_context = false)
+    results = nil
+    start_time = Time.now.to_i
+    p "Start cmd '#{cmd_string}'"
+    Open3.popen3(cmd_string) do |_stdin, stdout, stderr, _wait_thr|
+      output = stdout.read
+      output_error = stderr.read
+      handle_output_error(output_error)
+      results = parse_results(output, with_context)
+    end
+    end_time = Time.now.to_i
+    puts "Finished after: #{end_time - start_time} seconds"
 
-		return results
-	end
+    return results
+  end
 
-	def handle_output_error(output_error)
-		fail output_error if output_error && !output_error.empty?
-	end
+  def handle_output_error(output_error)
+    fail output_error if output_error && !output_error.empty?
+  end
 
-	def parse_results(results, _wait_thrh_context)
-		rows = results.gsub("\n--\n", "").split(/\r?\n|\r/)
+  def parse_results(results, _wait_thrh_context)
+    rows = []
+    begin
+      rows = results.gsub("\n--\n", "").split(/\r?\n|\r/)
+    rescue
+      results = results.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+      rows = results.gsub("\n--\n", "").split(/\r?\n|\r/)
+    end
 
-		if rows.length >= MAX_LIST_SIZE
-			rows << "\r\n WARNING: List truncated at #{MAX_LIST_SIZE} lines..."
-		end
+    if rows.length >= MAX_LIST_SIZE
+      rows << "\r\n WARNING: List truncated at #{MAX_LIST_SIZE} lines..."
+    end
 
-		return rows
-	end
+    return rows
+  end
 end
--- lib/summarizer.rb
+++ lib/summarizer.rb
@@ -1,14 +1,14 @@
 class Summarizer
-	def initialize
-		@log_folder = ENV['COMMONLOGS_ROOT_FORLDER']
+  def initialize
+    @log_folder = ENV['COMMONLOGS_ROOT_FOLDER']
 
-		fail "Must have Environment variable 'COMMONLOGS_ROOT_FORLDER' set.\n\n" unless @log_folder
+    fail "Must have Environment variable 'COMMONLOGS_ROOT_FOLDER' set.\n\n" unless @log_folder
 
-	end
+  end
 
-	def run
-		puts @log_folder
-	end
+  def run
+    puts @log_folder
+  end
 end
 
 Summarizer.new.run
