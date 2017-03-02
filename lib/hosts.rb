0ab11e6ef6c33c3255c559ec245517be1312222d
Blitline Developers <jason@blitline.com>
2017-03-02 15:52:14 -0800

    Added hosts listing

--- lib/hosts.rb
+++ lib/hosts.rb
@@ -4,14 +4,20 @@ HostInfo = Struct.new(:host, :tag, :timestamp)
 # Class to handle tags and tagged streams
 class Hosts
   def self.recent_data
-    filepath = CommonLog::Config.destination_folder + "/hosts"
     data = []
-    File.open(filepath).each do |line|
-      if line && !line.empty?
-        host_tag, timestamp = line.split(" ")
-        host, tag = host_tag.split(":")
-        data << HostInfo.new(host.strip, tag.strip, timestamp.to_i)
+    begin
+      filepath = CommonLog::Config.destination_folder + "/hosts"
+      File.open(filepath).each do |line|
+        if line && !line.empty?
+          host_tag, timestamp = line.split(" ")
+          host, tag = host_tag.split(":")
+          data << HostInfo.new(host.strip, tag.strip, timestamp.to_i)
+        end
       end
+
+      data.sort! { |x, y| y.timestamp <=> x.timestamp }
+    rescue => ex
+      puts "Failed to get hosts file #{ex.message}"
     end
     return data
   end
