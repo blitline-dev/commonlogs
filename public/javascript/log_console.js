e945bbe7d1c284194552f33aa1faeccec546604b
Jason GitKraken <jaciones@gmail.com>
2017-10-06 18:07:05 -0700

    touch cache

--- public/javascript/log_console.js
+++ public/javascript/log_console.js
@@ -50,7 +50,8 @@ LogConsole.prototype = {
 				try {
 					v = JSON.parse(rowText);
 					if (v) {
-						rowText = JSON.stringify(v, null, 2).replace(/\n/g, "<br/>");
+						rowText = JSON.stringify(v, null, 2);
+						rowText = "<pre>" + rowText + "</pre>";
 					}
 				}catch(ex) {
 					rowText = _this.escapeHtml(row[3].toString());
